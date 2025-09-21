ee6b54bd1def6303fe6b735472da1ab6a59bbe88
Patpatrouille <patrickgenitrini@gmail.com>
2025-09-21 12:14:08 +0200

    Init project vuejs+tailwind+daisyUI

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # vuejs-small-shop
+
+## init du projet vuejs + tailwindcss
+
+https://tailwindcss.com/docs/installation/using-vite
---
+++ small-shop/src/components/products/Cards.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Card from "@/components/products/Card.vue";
+
+const props = defineProps({
+    title: String,
+    products: Object
+})
+
+</script>
+
+<template>
+  <div class="w-full max-w-full p-3">
+    <h2 class="text-5xl font-bold mb-3">{{ title }}</h2>
+    <div class="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 space-x-6">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+    </div>
+  </div>
+</template>
