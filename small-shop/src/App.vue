ab7feaedfa03e8c59773d6f30ce41c40ed2952fc
Patpatrouille <patrickgenitrini@gmail.com>
2025-09-22 15:33:08 +0200

    App.vue from ignore?

---
+++ small-shop/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import DefaultLayout from '@/components/layouts/DefaultLayout.vue'
+</script>
+
+<template>
+    <DefaultLayout class="bg-base-300">
+        <router-view />
+    </DefaultLayout>
+</template>
+
+
