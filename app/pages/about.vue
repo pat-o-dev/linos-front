11c2dc7e37028883ecc249baefc6b069a4226e51
Patpatrouille <patrickgenitrini@gmail.com>
2025-09-28 18:34:28 +0200

    pages api begin

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,14 +1,17 @@
 <template>
   <section class="prose dark:prose-invert max-w-3xl mx-auto p-8">
-    <h1 class="text-3xl font-bold mb-6">À propos de Linos Store</h1>
-
+   
+    <div v-if="page">
+      <h1 class="text-3xl font-bold mb-6">{{ page.title }}</h1>
+      <div v-html="page.content"></div>
+    </div>
    
   </section>
 </template>
 
 <script lang="ts" setup>
 
-//const slug = "about"
-//const { page, error, pending } = usePage(slug);
+const slug = "about"
+const { page, error, pending } = usePage(slug);
 
 </script>
